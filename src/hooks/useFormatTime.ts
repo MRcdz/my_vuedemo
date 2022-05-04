695d00b0b30d3dfc226fb3eefa63d2990099ca7f
MrCdz <2463066771@qq.com>
2022-05-04 11:38:48 +0800

    clear

--- src/hooks/useFormatTime.ts
+++
@@ -1,7 +0,0 @@
-const useFormatTime = (time:Date) => {
-  const d = time.getFullYear() + '-' + (time.getMonth() + 1) + '-' + time.getDate()
-  const t = time.getHours() + ':' + time.getMinutes() + ':' + time.getSeconds()
-  const r = d + ' ' + t
-  return r
-}
-export default useFormatTime
